43efc5fff4786c796eb99e91ded96641963abf82
u4633849149-debug <u4633849149@gmail.com>
2026-03-08 09:58:46 +0100

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+let cart = JSON.parse(localStorage.getItem("cart")) || []
 
+function addToCart(name,price){
+
+cart.push({name,price})
+
+localStorage.setItem("cart",JSON.stringify(cart))
+
+alert("Produkt im Warenkorb")
+
+}
+
+function loadCart(){
+
+let list=document.getElementById("cartItems")
+
+let total=0
+
+cart.forEach(item=>{
+
+let li=document.createElement("li")
+
+li.innerText=item.name+" - "+item.price+"€"
+
+list.appendChild(li)
+
+total+=item.price
+
+})
+
+document.getElementById("total").innerText=total+"€"
+
+}
